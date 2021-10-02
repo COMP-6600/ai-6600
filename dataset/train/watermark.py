ecd50b510437ebee50a1396e58a7cd2eb19e48b9
Joshua Boyd <jaboydfamily@gmail.com>
2021-10-02 16:29:44 -0400

    added watermarker python script

--- dataset/train/watermark.py
+++
@@ -1,8 +0,0 @@
-from PIL import image, ImageDraw, ImageFont
-
-class WaterMarker()
-	def __init__():
-		pass
-
-	def displayCmdArgs():
-		
---
+++ dataset/train/watermarker.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw, ImageFont
+import sys
+
+class WaterMarker():
+	def __init__():
+		pass
+
+	def displayCmdArgs(args):
+		print(args)
+
+
+
+if __name__ == '__main__':
+	wm = WaterMarker
+	wm.displayCmdArgs(sys.argv)
