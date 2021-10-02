6837beb9d95d4f312f5ca08a2730020672d0ef39
Carlos Domani <carlos.a.domani@gmail.com>
2021-10-01 19:50:13 -0500

    Serve / and static files temporarily. Connect routes for model.

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,48 @@
 # Pull ASGI Server and FastAPI
-import fastapi.routing
 import uvicorn
+import mimetypes
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, Response
 
 # Load settings
-from app.core.config import logger
+from app.core.config import logger, settings
 
 # Load routers
 from app.api.auth import router as AUTH_ROUTER
+from app.api.model import router as MODEL_ROUTER
 
 # Main application
 app = FastAPI()
 
 # Routers
 app.include_router(AUTH_ROUTER)
+app.include_router(MODEL_ROUTER)
 
-# DEBUG: Display Routes that were loaded
-for route in app.routes:
-    if type(route) == fastapi.routing.APIRoute:
-        logger.debug(f"Route loaded: {route.path}")
+# Mount static directory to serve website
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
 
 
-# DEBUG: Index Route
-@app.get('/')
-def index():
-    return {'status': 'success', 'detail': 'Hello World!'}
+# TODO: Temporary solution, rely on aiofiles next time
+@app.get('/', response_class=HTMLResponse)
+async def index():
+    with open(str(settings.FRONTEND_PATH / "index.html")) as f:
+        page = f.read()
+    return page
+
+
+# TODO: Temporary solution, rely on aiofiles next time
+@app.get('/static/{static_file}')
+async def index(static_file):
+    file_mime = mimetypes.guess_type(str(settings.STATIC_PATH / static_file))
+    with open(str(settings.STATIC_PATH / static_file)) as f:
+        page = f.read()
+    return Response(content=page, media_type=file_mime[0])
 
 
 # DEBUG: Running module directly through uvicorn
 if __name__ == "__main__":
+    logger.debug("Server started from script.")
     uvicorn.run("main:app", port=5000, reload=True, log_level='debug', access_log=True)
