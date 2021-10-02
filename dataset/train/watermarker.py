036619b3fa85f3ef30736179b6de0813266b5c16
Joshua Boyd <jaboydfamily@gmail.com>
2021-10-02 17:19:29 -0400

    watermarker class successfuly applies watermark to passed image

--- dataset/train/watermarker.py
+++ dataset/train/watermarker.py
@@ -7,12 +7,12 @@ class WaterMarker():
 		self.currentImageName = None
 		self.currentDrawObj = None
 		self.watermarkText = "Test Watermark"
-		self.watermarkFont = ImageFont.truetype('arial.ttf', 82)
+		self.watermarkFont = ImageFont.truetype('/usr/share/fonts/gnu-free/FreeSans.otf', 82)
 
 	def drawMark(self, arg):
 		
 		# open image
-		self.currentImage = Image.open(arg)
+		self.currentImage = Image.open(r'./' + arg)
 		self.currentImageName = arg
 
 		# create draw object
@@ -22,22 +22,22 @@ class WaterMarker():
 		textWidth, textHeight = self.currentDrawObj.textsize(self.watermarkText,
 															 self.watermarkFont)
 		width, height = self.currentImage.size
-		x = width/2-textwidth/2
-		y = height-textheight-300
+		x = width/2-textWidth/2
+		y = height-textHeight-300
 
 		# draw watermark
 		self.currentDrawObj.text((x, y), self.watermarkText,
 								 font=self.watermarkFont)
 
 		# save image
-		self.currentImage.save(r'./' + self.currentImageName)
+		self.currentImage.save(r'./marked/' + self.currentImageName)
 		self.currentImage = None
 		self.currentImageName = None
 
 
 if __name__ == '__main__':
-	wm = WaterMarker
+	wm = WaterMarker()
 
 	# the first arg is the script name so we won't use that one
 	for count in range(1, len(sys.argv)):
-		print(sys.argv[count])
+		wm.drawMark(sys.argv[count])
